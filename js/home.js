f748ccd7cb62d64a889d5380d4934a2f54a649a5
Kaleb <kaleb@Kalebs-MacBook-Pro.local>
2023-07-12 14:07:53 -0400

    no js

--- js/home.js
+++ js/home.js
@@ -1,22 +1,20 @@
 const taglines = [
     ["ideas at work", "Ideas that Work."],
-    ["Proven Results", "Proven Performance"],
+    ["Proven Performance", "Proven Results"],
     ["The Practitioner's Perspective"]
 ]
 
 
 let running = true;
 
-while (running) {
-    taglines.forEach(line => {
-        setTimeout(() => {
-            if (line.length === 1) {
-                document.querySelector("h3").textContent = ""
-                document.querySelector("h2").textContent = line[0]
-            } else {
-                document.querySelector("h3").textContent = line[0]
-                document.querySelector("h2").textContent = line[1]
-            }
-        }, 12000);
-    })
-}
+taglines.forEach(line => {
+    setTimeout(() => {
+        if (line.length === 1) {
+            document.querySelector("h3").textContent = ""
+            document.querySelector("h2").textContent = line[0]
+        } else {
+            document.querySelector("h3").textContent = line[0]
+            document.querySelector("h2").textContent = line[1]
+        }
+    }, 12000);
+})
